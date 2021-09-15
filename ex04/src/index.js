c9535fd1ff64a6a2fd85f872fc6e03c0e0a4170f
Gordan Kranjcic <gordankranjcic@gmail.com>
2021-09-15 22:48:58 +0200

    Declaration of variables, Intialisation of var x and assigning variable x to y

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // Only change the code below this line
+    var x;
+    x = 10;
+    var y;
+    y = x;
+    // Only change the code below this line
+    return [x, y];
+}
+
+console.log(main());
+
+module.exports = main;
